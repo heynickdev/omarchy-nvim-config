e3df0e2fa4195d863e210a7a1dcc052ecbd41e3d
Nick Girling <nickgirling76@gmail.com>
2026-02-22 12:27:35 +0000

    backup before nuke

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,7 +3,7 @@ return {
   opts = function(_, opts)
     -- Ensure these parsers are always installed
     if type(opts.ensure_installed) == "table" then
-      vim.list_extend(opts.ensure_installed, { "templ", "go", "html" })
+      vim.list_extend(opts.ensure_installed, { "templ", "go", "html", "htmldjango", "css", "javascript" })
     end
 
     opts.indent = {
