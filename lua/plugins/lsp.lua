34ad485c60af844b6abb4c2c9604d9851dd0ebc1
Nick <nickgirling76@gmail.com>
2026-03-07 22:05:16 +0000

    batman

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,4 +1,3 @@
--- ~/.config/nvim/lua/plugins/lsp.lua
 return {
   {
     "neovim/nvim-lspconfig",
@@ -6,7 +5,21 @@ return {
       servers = {
         -- Existing servers
         angularls = { enabled = false },
-        gopls = {},
+        gopls = {
+          settings = {
+            gopls = {
+              -- This helps gopls understand how to handle whitespace during refactoring
+              ["formatting.gofumpt"] = false, -- gofumpt often forces tabs even harder
+            },
+          },
+          -- This forces the buffer to use spaces whenever a Go file is opened via LSP
+          on_attach = function(client, bufnr)
+            vim.bo[bufnr].expandtab = true
+            vim.bo[bufnr].shiftwidth = 2
+            vim.bo[bufnr].softtabstop = 2
+            vim.bo[bufnr].tabstop = 2
+          end,
+        },
 
         -- 1. Enable Templ LSP
         templ = {},
