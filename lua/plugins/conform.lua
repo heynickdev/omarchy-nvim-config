41f5a208279531d095a19834c128a30c8d6edc4c
heynickdev <nickgirling76@gmail.com>
2026-03-29 21:28:12 +0100

    batmans

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,14 +1,16 @@
 return {
   "stevearc/conform.nvim",
   opts = {
-    -- 1. Switch formatter to pg_format
+    -- 1. Added C, C++, and C# support (useful for university)
     formatters_by_ft = {
       sql = { "pg_format" },
-      -- vue = { "eslint_d" },
       htmldjango = { "djlint" },
+      cpp = { "clang_format" },
+      c = { "clang_format" },
+      cs = { "clang_format" },
     },
 
-    -- 2. Configure capitalization rules
+    -- 2. Configure capitalization and formatting rules
     formatters = {
       pg_format = {
         args = {
@@ -23,6 +25,17 @@ return {
           "-", -- Read from stdin
         },
       },
+      -- Customizing clang-format behavior globally
+      clang_format = {
+        prepend_args = {
+          "--style={IndentWidth: 4, BasedOnStyle: llvm, BreakBeforeBraces: Attach}",
+        },
+      },
+    },
+    -- 3. Ensure format on save is active
+    format_on_save = {
+      lsp_fallback = true,
+      timeout_ms = 500,
     },
   },
 }
